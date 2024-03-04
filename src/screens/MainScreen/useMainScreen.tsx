c46b38b46c6d3f03a88c109c76cdfd394a45876b
Franson33 <antonchikfr@gmail.com>
2024-03-04 13:58:31 +0200

    added fast image for better performance

--- src/screens/MainScreen/useMainScreen.tsx
+++ src/screens/MainScreen/useMainScreen.tsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useState} from 'react';
 import {ActivityIndicator, Image, Text, TouchableOpacity} from 'react-native';
+import FastImage from 'react-native-fast-image';
 import {useInfiniteQuery} from '@tanstack/react-query';
 import {useNavigation} from '@react-navigation/native';
 import type {NativeStackNavigationProp} from '@react-navigation/native-stack';
@@ -32,9 +33,12 @@ export const useMainScreen = () => {
     }
   };
 
-  const pressHandler = item => {
-    navigation.navigate(routeNames.DETAILS, {item});
-  };
+  const pressHandler = useCallback(
+    (item: any) => {
+      navigation.navigate(routeNames.DETAILS, {item});
+    },
+    [navigation],
+  );
 
   const footerItem = useCallback(
     () => (
@@ -47,25 +51,29 @@ export const useMainScreen = () => {
     [isFetching, isPagesNotEmpty],
   );
 
-  const renderItem = useCallback(({item}) => {
-    return (
-      <TouchableOpacity
-        style={styles.renderItemContainer}
-        onPress={() => pressHandler(item)}>
-        <Image
-          style={styles.renderItemImage}
-          source={{uri: item?.firstPreviewImage?.watermarked}}
-        />
-        <Text style={styles.renderItemText}>
-          {removeHtml(item?.title ?? '')}
-        </Text>
-        <Text style={styles.renderItemText}>
-          {item?.author?.details?.publicName}
-        </Text>
-        <Text style={styles.renderItemText}>{item?.price}</Text>
-      </TouchableOpacity>
-    );
-  }, []);
+  const renderItem = useCallback(
+    ({item}) => {
+      return (
+        <TouchableOpacity
+          style={styles.renderItemContainer}
+          onPress={() => pressHandler(item)}>
+          <FastImage
+            style={styles.renderItemImage}
+            source={{uri: item?.firstPreviewImage?.watermarked}}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+          <Text style={styles.renderItemText}>
+            {removeHtml(item?.title ?? '')}
+          </Text>
+          <Text style={styles.renderItemText}>
+            {item?.author?.details?.publicName}
+          </Text>
+          <Text style={styles.renderItemText}>{item?.price}</Text>
+        </TouchableOpacity>
+      );
+    },
+    [pressHandler],
+  );
 
   return {
     items: data?.pages?.flatMap(page => page?.items?.materials || []),
